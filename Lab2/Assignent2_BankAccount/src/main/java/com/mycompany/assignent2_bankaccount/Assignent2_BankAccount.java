ef26b0de13330004661d4dbc0c66d7a649323f1d
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-05-04 09:31:39 +0700

    Lab2as2

---
+++ Lab2/Assignent2_BankAccount/src/main/java/com/mycompany/assignent2_bankaccount/Assignent2_BankAccount.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignent2_bankaccount;
+
+/**
+ *
+ * @author Dell
+ */
+public class Assignent2_BankAccount {
+
+    public static void main(String[] args) {
+       // System.out.println("Hello World!");
+    }
+}
