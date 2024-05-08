fe1e241f1016473a34938ee2e976a91e1e364dc7
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-05-08 21:57:52 +0700

    Update Assighment1_ComplexNumber.java

--- Lab2/Assighment1_ComplexNumber/src/main/java/com/mycompany/assighment1_complexnumber/Assighment1_ComplexNumber.java
+++ Lab2/Assighment1_ComplexNumber/src/main/java/com/mycompany/assighment1_complexnumber/Assighment1_ComplexNumber.java
@@ -32,6 +32,5 @@ public class Assighment1_ComplexNumber {
 
         System.out.print("So phuc sau khi chia la: ");
         complexNumber1.divFunction(complexNumber2).Output();
-        //test
     }
 }
