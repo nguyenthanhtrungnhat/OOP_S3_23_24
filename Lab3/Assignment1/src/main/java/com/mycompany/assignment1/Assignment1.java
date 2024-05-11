484be97978f23edea3b48b100855c3ef2e492541
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-05-11 09:15:47 +0700

    Lab3

--- Lab3/Assignment1/src/main/java/com/mycompany/assignment1/Assignment1.java
+++ Lab3/Assignment1/src/main/java/com/mycompany/assignment1/Assignment1.java
@@ -4,6 +4,7 @@
 package com.mycompany.assignment1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -13,6 +14,57 @@ public class Assignment1 {
 
     public static void main(String[] args) {
         ArrayList<CD> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
         CD cd = new CD();
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("1. Add CD");
+            System.out.println("2. Search CD by CD title");
+            System.out.println("3. Search CDs by collection");
+            System.out.println("4. Search CDs by type");
+            System.out.println("5. Delete CD by CD Id");
+            System.out.println("6. Edit CD information by Id");
+            System.out.println("7. Display all CDs");
+            System.out.println("8. Sort the CD list ascending by year of release");
+            System.out.println("9. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 1:
+                    cd.addCD();
+                    break;
+                case 2:
+                    System.out.print("Enter CD title to search: ");
+                    cd.searchByTitle();
+                    break;
+                case 3:
+                    System.out.print("Enter CD collection to search: ");
+                    cd.searchByCollection();
+                    break;
+                case 4:
+                    System.out.print("Enter CD type to search (audio/video): ");
+                    cd.searchByType();
+                    break;
+                case 5:
+                    System.out.print("Enter CD Id to delete: ");
+                    cd.deleteCdById();
+                    break;
+                case 6:
+                    cd.editInformationById();
+                    break;
+                case 7:
+                    System.out.println("List of CDs: ");
+                    cd.DisplayAllCds();
+                    break;
+                case 8:
+                    cd.sortByYor();
+                    break;
+                case 9:
+                    cd.Exit();
+                    break;
+
+            }
+        }
     }
 }
--- Lab3/Assignment1/src/main/java/com/mycompany/assignment1/CD.java
+++ Lab3/Assignment1/src/main/java/com/mycompany/assignment1/CD.java
@@ -5,6 +5,8 @@
 package com.mycompany.assignment1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,6 +23,7 @@ public class CD {
     private double price;
     private int yearOfRelease;
     private List<CD> list = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
 
     public CD() {
     }
@@ -34,17 +37,22 @@ public class CD {
         this.yearOfRelease = yearOfRelease;
     }
 
-    public void Input() {
-        Scanner sc = new Scanner(System.in);
+    public void addCD() {
+
         System.out.print("How many CDs: ");
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            System.out.println("");
+            System.out.print("Id: ");
             id = sc.next();
+            System.out.print("Collection: ");
             collcetion = sc.next();
+            System.out.print("Type: ");
             type = sc.next();
+            System.out.print("Title: ");
             title = sc.next();
+            System.out.print("Price: ");
             price = sc.nextDouble();
+            System.out.print("Year of release: ");
             yearOfRelease = sc.nextInt();
             CD cd = new CD(id, collcetion, type, title, price, yearOfRelease);
             list.add(cd);
@@ -52,37 +60,80 @@ public class CD {
     }
 
     public void Exit() {
-        
+        System.out.println("Exiting...");
+        System.exit(0);
     }
 
     public void sortByYor() {
+        list.sort((p1, p2) -> {
+            return p1.yearOfRelease - p2.yearOfRelease;
 
+        });
     }
 
     public void editInformationById() {
-
+        System.out.print("Enter Id of CD you want to edit data: ");
+        String temp = sc.next();
+        for (CD each : list) {
+            if (each.id.equals(temp)) {
+                each.collcetion = sc.next();
+                each.type = sc.next();
+                each.title = sc.next();
+                each.price = sc.nextDouble();
+                each.yearOfRelease = sc.nextInt();
+                System.out.println("ID: " + each.id + " ,Collection: " + each.collcetion + " ,Type: " + each.type + " ,Title: " + each.title + " ,Price: " + each.price + " ,Year of release: " + each.yearOfRelease);
+                break;
+            }
+        }
     }
 
     public void searchByType() {
-
+        System.out.print("Enter type of CD: ");
+        String findByType = sc.next();
+        for (CD each : list) {
+            if (each.title.equals(findByType)) {
+                System.out.println("ID: " + each.id + " ,Collection: " + each.collcetion + " ,Type: " + each.type + " ,Title: " + each.title + " ,Price: " + each.price + " ,Year of release: " + each.yearOfRelease);
+            }
+        }
     }
 
     public void searchByCollection() {
-
+        System.out.print("Enter collection of CD: ");
+        String findByCollection = sc.next();
+        for (CD each : list) {
+            if (each.title.equals(findByCollection)) {
+                System.out.println("ID: " + each.id + " ,Collection: " + each.collcetion + " ,Type: " + each.type + " ,Title: " + each.title + " ,Price: " + each.price + " ,Year of release: " + each.yearOfRelease);
+            }
+        }
     }
 
     public void searchByTitle() {
-
+        System.out.print("Enter name of CD: ");
+        String findByName = sc.next();
+        for (CD each : list) {
+            if (each.title.equals(findByName)) {
+                System.out.println("ID: " + each.id + " ,Collection: " + each.collcetion + " ,Type: " + each.type + " ,Title: " + each.title + " ,Price: " + each.price + " ,Year of release: " + each.yearOfRelease);
+            }
+        }
     }
 
     public void deleteCdById() {
+        System.out.print("Enter Id of CD you want to delete data: ");
+        String temp = sc.next();
+        for (CD each : list) {
+            if (each.id.equals(temp)) {
+                list.remove(each);
+                System.out.println("Deleted CD having id: " + each.id);
+                break;
+            }
+        }
 
     }
 
     public void DisplayAllCds() {
+        System.out.println("All CDs: ");
         for (CD each : list) {
-            System.out.println("All CDs: ");
-            System.out.println(each.id + " " + each.collcetion + " " + each.type + " " + each.title + " " + each.price + " " + each.yearOfRelease);
+            System.out.println("ID: " + each.id + " ,Collection: " + each.collcetion + " ,Type: " + each.type + " ,Title: " + each.title + " ,Price: " + each.price + " ,Year of release: " + each.yearOfRelease);
         }
     }
 
