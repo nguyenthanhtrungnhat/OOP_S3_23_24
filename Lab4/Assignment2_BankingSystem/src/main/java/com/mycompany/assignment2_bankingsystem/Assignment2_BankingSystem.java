c8171d39289760e31f7fa0b1498d9a4cce5ace50
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-05-18 08:43:22 +0700

    Lab4as2

---
+++ Lab4/Assignment2_BankingSystem/src/main/java/com/mycompany/assignment2_bankingsystem/Assignment2_BankingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment2_bankingsystem;
+
+/**
+ *
+ * @author Dell
+ */
+public class Assignment2_BankingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
