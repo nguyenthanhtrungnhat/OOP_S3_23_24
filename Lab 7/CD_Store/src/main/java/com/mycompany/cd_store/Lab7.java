aa61ddcb01239c3a229e0cc8758360e4cfe995a0
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-06-08 08:23:17 +0700

    Update

---
+++ Lab 7/CD_Store/src/main/java/com/mycompany/cd_store/Lab7.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cd_store;
+
+/**
+ *
+ * @author Dell
+ */
+public class Lab7 {
+
+    public static void main(String[] args) {
+        CD_Store c=new CD_Store();
+        c.setVisible(true);
+    }
+}
