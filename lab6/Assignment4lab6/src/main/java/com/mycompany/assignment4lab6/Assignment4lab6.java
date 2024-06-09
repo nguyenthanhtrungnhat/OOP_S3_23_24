9c0abac3eef3d4a59430fc97c7702d5cbb60e134
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-06-10 00:06:32 +0700

    Update

---
+++ lab6/Assignment4lab6/src/main/java/com/mycompany/assignment4lab6/Assignment4lab6.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment4lab6;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Dell
+ */
+public class Assignment4lab6 {
+
+    public static void main(String[] args) {
+        JFrame j =new JFrame();
+        j.setVisible(true);
+    }
+}
